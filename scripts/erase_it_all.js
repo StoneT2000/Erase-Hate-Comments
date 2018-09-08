35b77945e53a6e560246364df03ca5894e35f506
StoneT2000 <stoneztao@gmail.com>
2018-09-09 00:40:39 +0800

    Initial commit

---
+++ scripts/erase_it_all.js
@@ -0,0 +1,69 @@
+var ccanvas;
+var cWidth = window.innerWidth;
+var cHeight = window.innerHeight;
+var cursorcanvas;
+var commentscanvas;
+var bgcolor = "rgb(250,250,250)";
+
+
+function setup() {
+  ccanvas = createCanvas(cWidth, cHeight);
+  ccanvas.parent('ccanvas');
+  
+  //Create commentscanvas, where all text is put up
+  commentscanvas = createGraphics(cWidth, cHeight)
+  cursorcanvas = createGraphics(cWidth, cHeight)
+  generate_comments(commentscanvas)
+}
+
+var erase_point = [[0,0],[0,0]];
+var pos = 1;
+function draw() {
+  background(bgcolor);
+  //Have an eraser as a cursor
+  //cursorcanvas.clear();
+  image(commentscanvas, 0, 0)
+  noFill();
+  stroke(1);
+  ellipse(mouseX, mouseY, 30, 30)
+  if (mouseIsPressed) {
+    erase_point[pos] = [mouseX,mouseY]
+    if (pos == 1) {
+      pos = 0;
+    }
+    else {
+      pos = 1;
+    }
+    //Erase in line between two points?
+    commentscanvas.stroke(bgcolor);
+    commentscanvas.strokeWeight(30)
+    commentscanvas.line(erase_point[0][0],erase_point[0][1],erase_point[1][0],erase_point[1][1])
+  }
+  else {
+    erase_point[pos] = [mouseX,mouseY]
+    if (pos == 1) {
+      pos = 0;
+    }
+    else {
+      pos = 1;
+    }
+  }
+  
+}
+
+//Generate random comments onto canvas context ctx
+function generate_comments(ctx) {
+  
+  for (var i = 0; i < 10; i++) {
+    ctx.textSize(random(20,50));
+    var randomWord = hate_comments[floor(random(0,hate_comments.length))];
+    ctx.text(randomWord, random(10, cWidth-100), random(10,cHeight-100));
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  cursorcanvas = createGraphics(cWidth, cHeight)
+}
+
+var hate_comments = ['Your content sucks', 'Go away', 'Get a life', 'You suck', 'Hater was here', 'You got played', 'Go somewhere else', 'Trash', 'Peasant', 'Bully', 'Muppet', 'Muggle', 'Haters gonna hate', 'Stop trying', 'Get out', 'Disappointment', 'Try harder', '']
